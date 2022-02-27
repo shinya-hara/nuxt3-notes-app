cb56b074299e775222d19d9ddd5e2e87359a2e02
Shinya Hara <s.hara@sonicmoov.com>
2022-02-27 23:02:06 +0900

    chore: Update build target

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,5 @@ import { defineNuxtConfig } from 'nuxt3'
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
-  target: 'static',
   css: ['quasar/dist/quasar.prod.css'],
 })

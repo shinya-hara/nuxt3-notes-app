2ec6811d998f9d7b5daa3c5d60a3d8e21ccfca44
Shinya Hara <rabby.coffee.c5@gmail.com>
2022-03-05 14:07:22 +0900

    chore: title, meta設定

--- app.vue
+++ app.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-  </div>
+  <Html>
+    <Head>
+      <Title>Nuxt3 Notes App</Title>
+      <Meta name="robots" content="noindex,nofollow" />
+    </Head>
+
+    <Body>
+      <NuxtLayout>
+        <NuxtPage />
+      </NuxtLayout>
+    </Body>
+  </Html>
 </template>
